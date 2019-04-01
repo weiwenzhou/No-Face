6f71694c98959347aeece2f296addc263584618f
wzhou2 <wzhou2@stuy.edu>
2019-04-01 13:19:40 -0400

    begin layout

---
+++ static/index.js
@@ -0,0 +1,31 @@
+// Variables
+var data_link;
+
+// SVG Variables
+var WIDTH;
+var HEIGHT;
+var margin; // dictionary
+
+// OPEN DATA
+d3.csv(data_link function(table) {
+    // Format data here
+}).then(function(data) {
+    // Put stuff here
+
+    // SCALE POPULATION to display an appropriate amount of people
+    var p_scale;
+
+    // Draw countries?
+    // Initialize the data at 1800
+
+
+    // Create a timeline (1800 - 2100)
+
+
+    // Add title
+
+
+    // Transition
+    
+
+});
