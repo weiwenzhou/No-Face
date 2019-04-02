987a736f9189b001aa8b7b86a3ca407e662bee65
wzhou2 <wzhou2@stuy.edu>
2019-04-02 13:13:53 -0400

    the data looks so dirty or the map is ugly notation

--- util/parser.py
+++ util/parser.py
@@ -1,3 +1,5 @@
+import csv
+# World Map
 f = open("../static/world.svg")
 lines = f.readlines()
 names = []
@@ -9,4 +11,26 @@ while c < len(lines):
     c += 1
 
 print (len(names))
+# print (names)
+
+# Population data
+countries = []
+excluded  = []
+with open("../data/population_1800-2100.csv") as population:
+    table = csv.DictReader(population)
+    for row in table:
+        if row['name'] in names:
+            countries.append(row['name'])
+            names.remove(row['name'])
+        else:
+            excluded.append(row['name'])
+names = sorted(names)
+excluded = sorted(excluded)
+print (len(names))
 print (names)
+print ("===========================================")
+print(excluded)
+print (names.index("Central African Rep."))
+# Notes
+# two congos in population
+# ["Bahrain", "Holy See", "Kiribati", "Marshall Island", "Micronesia", "Monaco", "Samoa", "San Marino", "Sao Tome and Principe", "St. Lucia", "Tuvalu"]
